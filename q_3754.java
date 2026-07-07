b1fc88b43e685e109b110e120b0650b854cddd5e
saijyo18 <saijyothi1206@gmail.com>
2026-07-07 21:36:04 +0530

    today's sum

---
+++ q_3754.java
@@ -0,0 +1,19 @@
+class q_3754 {
+    public long sumAndMultiply(int n) {
+        long x = 0;
+        long place = 1;
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                x += (long) digit * place;
+                place *= 10;
+                sum += digit;
+            }
+            n /= 10;
+        }
+
+        return x * sum;
+    }
+}
